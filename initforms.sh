28049915ad3d7e194a842c0ac399a16c08589b5d
Charlie Root <root@clonos.my.domain>
2018-11-22 02:27:27 +0100

    wip

--- initforms.sh
+++ initforms.sh
@@ -23,9 +23,6 @@ if [ ! -r ${SALT_FILE} ]; then
 	chown web:cbsd ${SALT_FILE}
 fi
 
-#[ ! -d "${MYPATH}" ] && err 1 "No such ${MYPATH}"
-#[ -f "${MYPATH}/${HELPER}.sqlite" ] && /bin/rm -f "${MYPATH}/${HELPER}.sqlite"
-
 # sys_helpers_list, jails_helper_wl
 /usr/local/bin/cbsd ${miscdir}/updatesql ${DBFILE} ${MYPATH}/sys_helpers_list.schema sys_helpers_list
 /usr/local/bin/cbsd ${miscdir}/updatesql ${DBFILE} ${MYPATH}/sys_helpers_list.schema jails_helpers_list
@@ -73,16 +70,10 @@ admin_user=$( /usr/local/bin/sqlite3 ${DBFILE} "SELECT username FROM auth_user" 
 if [ -z "${admin_user}" ]; then
 	SALT=$( cat ${SALT_FILE} |awk '{printf $1}' )
 	echo "Add new admin user: admin/admin with salt: ${SALT}"
-	#$password = 'password';
-	#hash1 = sha256($password);
-	#$salt="kae1Pu4eic3oji4IDen0";
-	#$saltedHash = sha256($hash1.$salt);
 	echo ${SALT} > ${SALT_FILE}
-
 	password="admin"
 	hash1=$( sha256 -qs "${password}" )
 	hash2="${hash1}${SALT}"
-	echo "HASH2 ${hash2}"
 	salted_hash=$( sha256 -qs "${hash2}" )
 
 /usr/local/bin/sqlite3 ${DBFILE} << EOF
@@ -92,4 +83,4 @@ COMMIT;
 EOF
 fi
 
-chown web:web ${DBFILE}
+chown www:www ${DBFILE}
